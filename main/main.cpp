44f72d125d26917dd1c0d5de05d2329d05027a40
Arman <uarman2215@gmail.com>
2022-10-08 19:08:45 -0400

    added sql poc

--- main/main.cpp
+++ main/main.cpp
@@ -2,8 +2,70 @@
 #include "crow/crow_all.h"
 #include <iostream>
 #include <string>
+#include <stdlib.h>
+
+#include "mysql_connection.h"
+
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+#include <cppconn/prepared_statement.h>
+
+using namespace std;
 
 int main(int argc, char** argv) {
+	cout << endl;
+	cout << "Let's have MySQL count from 10 to 1..." << endl;
+
+	try {
+	  sql::Driver *driver;
+	  sql::Connection *con;
+	  sql::Statement *stmt;
+	  sql::ResultSet *res;
+	  sql::PreparedStatement *pstmt;
+
+	  /* Create a connection */
+	  driver = get_driver_instance();
+	  con = driver->connect("tcp://127.0.0.1:3306", "dbuser", "123");
+	  /* Connect to the MySQL test database */
+	  con->setSchema("test");
+
+	  stmt = con->createStatement();
+	  stmt->execute("DROP TABLE IF EXISTS test");
+	  stmt->execute("CREATE TABLE test(id INT)");
+	  delete stmt;
+
+	  /* '?' is the supported placeholder syntax */
+	  pstmt = con->prepareStatement("INSERT INTO test(id) VALUES (?)");
+	  for (int i = 1; i <= 10; i++) {
+		pstmt->setInt(1, i);
+		pstmt->executeUpdate();
+	  }
+	  delete pstmt;
+
+	  /* Select in ascending order */
+	  pstmt = con->prepareStatement("SELECT id FROM test ORDER BY id ASC");
+	  res = pstmt->executeQuery();
+
+	  /* Fetch in reverse = descending order! */
+	  res->afterLast();
+	  while (res->previous())
+		cout << "\t... MySQL counts: " << res->getInt("id") << endl;
+	  delete res;
+
+	  delete pstmt;
+	  delete con;
+
+	} catch (sql::SQLException &e) {
+	  cout << "# ERR: SQLException in " << __FILE__;
+	  cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
+	  cout << "# ERR: " << e.what();
+	  cout << " (MySQL error code: " << e.getErrorCode();
+	  cout << ", SQLState: " << e.getSQLState() << " )" << endl;
+	}
+
+	cout << endl;
     crow::SimpleApp app; //define your crow application
 
     //define your endpoint at the root directory
@@ -13,7 +75,7 @@ int main(int argc, char** argv) {
 
     //set the port, set the app to run on multiple threads, and run the app
     app.port(18080).multithreaded().run();
-  return 0;
+	return 0;
 }
 
 
