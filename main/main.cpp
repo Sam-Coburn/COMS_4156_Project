c3190de67707dd7a8b1df150c4b75018a34fe975
Arman <uarman2215@gmail.com>
2022-10-08 18:59:39 -0400

    Crow POC

---
+++ main/main.cpp
@@ -0,0 +1,19 @@
+
+#include "crow/crow_all.h"
+#include <iostream>
+#include <string>
+
+int main(int argc, char** argv) {
+    crow::SimpleApp app; //define your crow application
+
+    //define your endpoint at the root directory
+    CROW_ROUTE(app, "/")([](){
+        return "Hello world";
+    });
+
+    //set the port, set the app to run on multiple threads, and run the app
+    app.port(18080).multithreaded().run();
+  return 0;
+}
+
+
